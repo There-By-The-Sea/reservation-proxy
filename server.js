1bc5bdea5e23e152a90836a1fda725246e274eb3
Trevor Bell <trevor2355@gmail.com>
2020-03-25 11:46:11 -0600

    use http-proxy-middleware package to handle api routes more effectively

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = 3000;
@@ -10,7 +11,32 @@ app.use(morgan('dev'));
 //serve up the react client
 app.use(express.static('client'));
 
-
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+
+//[[[[[[[[[[[[[[[[[[[[[ RESERVATION FROM API ROUTES ]]]]]]]]]]]]]]]]]]]]]
+
+app.use('/reservations/plan', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrgin: true
+}));
+
+app.use('/reservations/book', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrgin: true
+}));
+
+
+//[[[[[[[[[[[[[[[[[[[[[ REVIEWS API ROUTES ]]]]]]]]]]]]]]]]]]]]]
+
+app.use('/api/search', createProxyMiddleware({
+  target: 'http://localhost:3003',
+  changeOrgin: true
+}));
+
+app.use('/api/reviews', createProxyMiddleware({
+  target: 'http://localhost:3003',
+  changeOrgin: true
+}));
